6f2290a32dd1ed1572ced162d3d2c3c514a5aa9a
Luca <luca@informaten.com>
2024-01-28 17:59:36 +0100

    domain prcing test

--- lib/Resolver.php
+++ lib/Resolver.php
@@ -2,6 +2,8 @@
 
 namespace WHMCS\Module\Addon\DomainInfoAPI;
 
+use WHMCS\Database\Capsule;
+
 class Resolver {
 
     private function createJSONResponse($data, $status = 200) {
@@ -14,12 +16,52 @@ class Resolver {
         return $this->createJSONResponse(['status' => 'error', 'message' => 'Not Found'], 404);
     }
 
+    function domainPricing($params) {
+        $tlds_registration = Capsule::table('tbldomainpricing')
+            ->join('tblpricing', 'tbldomainpricing.id', '=', 'tblpricing.relid')
+            ->select('tbldomainpricing.id', 'tbldomainpricing.extension', 'tblpricing.msetupfee')
+            ->where('tblpricing.type', '=', 'domainregister')
+            ->get();
+        $tlds_transfer = Capsule::table('tbldomainpricing')
+            ->join('tblpricing', 'tbldomainpricing.id', '=', 'tblpricing.relid')
+            ->select('tbldomainpricing.id', 'tbldomainpricing.extension', 'tblpricing.msetupfee')
+            ->where('tblpricing.type', '=', 'domaintransfer')
+            ->get();
+        // above query only returns the transfer price, so we need to get the renew prices as well
+        $tlds_renew = Capsule::table('tbldomainpricing')
+            ->join('tblpricing', 'tbldomainpricing.id', '=', 'tblpricing.relid')
+            ->select('tbldomainpricing.id', 'tbldomainpricing.extension', 'tblpricing.msetupfee')
+            ->where('tblpricing.type', '=', 'domainrenew')
+            ->get();
+
+
+
+
+
+        $items = [];
+        foreach ($tlds_registration as $tld) {
+            $items[] = [
+                'tld' => $tld->extension,
+                'registration' => $tld->msetupfee,
+                'transfer' => $tlds_transfer->where('extension', $tld->extension)->first()->msetupfee,
+                'renew' => $tlds_renew->where('extension', $tld->extension)->first()->msetupfee,
+            ];
+        }
+        
+        $result = [
+            'status' => 'success',
+            'items' => $items,
+        ];
+        
+        return $this->createJSONResponse($result);
+    }
+
     function resolve($params) {
         $endpoint = $params['endpoint'];
 
         switch ($endpoint) {
             case 'pricing':
-                return $this->createJSONResponse(['status' => 'success', 'data' => ['domain' => ['com' => ['register' => 10.99, 'renew' => 10.99, 'transfer' => 10.99]]]]);
+                return $this->domainPricing($params);
         }
 
         return $this->notFound();
