778a6a53ba771005c37e499aac7f370656367f74
Luca <luca@informaten.com>
2024-01-28 16:40:43 +0100

    initial commit

---
+++ domaininfoapi.php
@@ -0,0 +1,43 @@
+<?php
+
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}
+
+
+function domaininfoapi_config()
+{
+    return [
+        'name' => 'Domain Info API',
+        'description' => 'Adds an API to query domain information from outside WHMCS.',
+        'author' => 'Luca Drefke',
+        'language' => 'english',
+        'version' => '1.0',
+        'fields' => []
+    ];
+}
+
+
+function domaininfoapi_activate() {
+    return [
+        'status' => 'success',
+    ];
+}
+
+function domaininfoapi_deactivate() {
+    return [
+        'status' => 'success',
+    ];
+}
+
+// function for the public api
+function domaininfoapi_clientarea($vars) {
+
+    return [
+        'status' => 'success',
+    ];
+
+}
+
+
+
